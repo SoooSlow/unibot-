ddf808f9d4e156001fafe04a36caf28a86340a5c
SoooSlow <38427033+SoooSlow@users.noreply.github.com>
2018-07-22 13:17:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -30,43 +30,10 @@ clientDiscord.on("message", message => {
             .setFooter(botname + " Private Edition || " + version)
             .addField(prefix + "help", "Affiche la page d'aide.", false)
             .addField(prefix + "addbot", "Ajouter le bot sur votre serveur. (Non disponible)", false)
-            .addField(prefix + "logs on/off/status", "Activer ou désactiver les logs. (Permissions => ADMINISTRATOR)", false)
-            .addField(prefix + "logsid", "Changer l'id du channel des logs. (Permissions => ADMINISTRATOR)", false)
-            .addField(prefix + "prefix", "Changer le prefix du bot. (Permissions => ADMINISTRATOR)", false)
             .setColor("0x000000");
         message.channel.sendEmbed(helppage);
     }
     if (command === "addbot"){
         message.reply("Commande indisponible pour le moment !")
-    }
-    if(message.member.hasPermission("ADMINISTRATOR")){
-        if (command === "logs"){
-            if (args.length === 2){
-                if (args[1] === "on") logs = true & message.channel.sendMessage("Les logs Unilogs sont désormais activés !");
-                if(args[1] === "off") logs = false & message.channel.sendMessage("Les logs Unilogs sont désormais désactivés !");
-                if(args[1] === "status"){
-                    if(logs === true){ 
-                        message.channel.sendMessage("Les logs Unilogs sont activés.");
-                    }
-                    else message.channel.sendMessage("Les logs Unilogs sont désactivés.");
-                }
-            }
-            else message.reply("Dépassement des arguments !");
-        }
-        if(command === "logsid"){
-            if (args.length === 2){
-                logchannelid = args[1]
-                message.channel.sendMessage("Les logs se feront désormais dans le channel <#" + logchannelid + "> !");
-            }
-            else message.reply("Dépassement des arguments !");
-        }
-        if (command === "prefix"){
-            if (args.length === 2){
-                prefix = args[1];
-                message.channel.sendMessage("Le prefix de " + botname + " est désormais " + args[1] + " !");
-                clientDiscord.user.setGame(prefix + "help || " + clientDiscord.guilds.size + " serveurs");
-            }
-            else message.reply("Dépassement des arguments !");
-        }
     }
 });
