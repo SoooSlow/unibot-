d342fe0c9d25e893cf3b60579210330f97e5a7e0
SoooSlow <38427033+SoooSlow@users.noreply.github.com>
2018-07-22 11:38:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const discord = require("discord.js");
 const clientDiscord = new discord.Client();
 
 /* VARIABLES */
+var logs = false;
 const prefix = "§";
 const token = process.env.KEY;
 const botname = "UniBot";
@@ -19,13 +20,19 @@ clientDiscord.on("ready", () => {
 
 clientDiscord.on("message", message => {
     if (message.content === prefix + "help"){
-        var helppage = new discord.MessageEmbed()
+        if(logs){
+            
+        }
+        var helppage = new discord.RichEmbed()
             .setTitle("Aide")
-            .setDescription(botname + " Private Edition | " + version)
+            .setDescription("Voici la liste des commandes !")
+            .setFooter(botname + " Private Edition || " + version)
             .addField("§help", "Affiche la page d'aide.", false)
-            .addField("command", "text", false)
-            .setColor("000000");
-        channel.sendMessage(helppage);
+            .addField("§addbot", "Ajouter le bot sur votre serveur. (Non disponible)", false)
+            .setColor("0x000000");
+        message.channel.sendEmbed(helppage);
     }
-
+    if (message.content === prefix + "addbot"){
+        message.reply("Cette commande n'est pas encore disponible !");
+    }
 });
